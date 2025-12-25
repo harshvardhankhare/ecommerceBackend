cbd6df2f64a0449c124daa7061443846b8a810c2
harshvardhankhare <khareharsh543@gmail.com>
2025-12-25 20:41:16 +0530

    Initial Spring Boot backend

---
+++ src/main/java/com/myFirstProject/myFirstProject/Service/ProductService.java
@@ -0,0 +1,57 @@
+package com.myFirstProject.myFirstProject.Service;
+
+import com.myFirstProject.myFirstProject.DTO.ProductRequestDto;
+import com.myFirstProject.myFirstProject.Repository.ProductRepository;
+import com.myFirstProject.myFirstProject.entity.Products;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public  void insertProduct(ProductRequestDto pr){
+        Products p = new Products();
+        p.setProduct_name(pr.getName());
+        p.setDiscount(pr.getDiscount());
+        p.setCategory(pr.getCategory());
+        p.setProduct_price(pr.getPrice().intValue());
+        p.setSlug(pr.getSlug());
+        p.setImgLink(pr.getImg());
+        p.setProduct_desc(pr.getDesc());
+
+        productRepository.save(p);
+
+    }
+
+    public Optional<Products> getPrductById(int id){
+     Optional<Products> product = productRepository.findById(id);
+
+     if(product.isPresent()){
+         return product;
+     }else {
+         return null;
+     }
+    }
+
+    public List<Products> getAllProducts(){
+
+     return   productRepository.findAll();
+    }
+
+    public Products getProductById(int Id){
+
+        return productRepository.findById(Id).orElseThrow(()-> new RuntimeException(" Product Not Found"));
+    }
+
+
+    public List<Products> getSearchProduct(String q) {
+
+        return productRepository.searchByProductName(q);
+    }
+}
---
+++ src/main/java/com/myFirstProject/myFirstProject/utility/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.myFirstProject.myFirstProject.utility;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET = "mySecretKeyaksdnn4b35bbj2k339rjwinfjwfbeibebgeuebrdbgddfjbjgnjsfkjsfkd ";
+
+    // 🔹 Generate token using USER ID
+    public String generateToken(Long userId) {
+
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId)) // USER ID stored here
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 1000 * 60 * 60)
+                )
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+
+    // 🔹 Extract USER ID from token
+    public Long extractUserId(String token) {
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
+
+    // 🔹 Check if token expired
+    public boolean isTokenExpired(String token) {
+
+        Date expiration = Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+
+        return expiration.before(new Date());
+    }
+}
