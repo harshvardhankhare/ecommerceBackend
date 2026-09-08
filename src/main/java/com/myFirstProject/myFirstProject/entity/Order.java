7fdd3b26c708e7efe8cdde9a2f64d4c11da6b708
harshvardhankhare <khareharsh543@gmail.com>
2026-09-08 21:26:00 +0530

    product enhanced and varies other improvements

--- src/main/java/com/myFirstProject/myFirstProject/entity/Order.java
+++ src/main/java/com/myFirstProject/myFirstProject/entity/Order.java
@@ -25,7 +25,7 @@ public class Order {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> items;
 
-    private int totalPrice;
+    private double totalPrice;
 
     private String status;
     // CREATED, PAID, SHIPPED, DELIVERED, CANCELLED
